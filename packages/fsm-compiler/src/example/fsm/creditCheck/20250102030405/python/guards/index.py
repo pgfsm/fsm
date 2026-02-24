ff9bf873bc45795717571be72daba3a4975070da
Niraj Kashyap <niraj38re@gmail.com>
2026-02-24 23:33:22 +0530

    Run: genrateFsmPluginFromFolder deno fn

---
+++ packages/fsm-compiler/src/example/fsm/creditCheck/20250102030405/python/guards/index.py
@@ -0,0 +1,20 @@
+# Guard: allSucceeded
+def allSucceeded(context, event):
+    # TODO: implement
+    return True
+
+# Guard: equiGavinReportFound
+def equiGavinReportFound(context, event):
+    # TODO: implement
+    return True
+
+# Guard: gavUnionReportFound
+def gavUnionReportFound(context, event):
+    # TODO: implement
+    return True
+
+# Guard: gavperianReportFound
+def gavperianReportFound(context, event):
+    # TODO: implement
+    return True
+
---
+++ packages/fsm-compiler/src/example/fsm/creditCheck/20250102030405/typescript/guards/index.ts
@@ -0,0 +1,24 @@
+// Guard: allSucceeded
+export function allSucceeded(context: any, event: any) {
+  // TODO: implement
+  return true;
+}
+
+// Guard: equiGavinReportFound
+export function equiGavinReportFound(context: any, event: any) {
+  // TODO: implement
+  return true;
+}
+
+// Guard: gavUnionReportFound
+export function gavUnionReportFound(context: any, event: any) {
+  // TODO: implement
+  return true;
+}
+
+// Guard: gavperianReportFound
+export function gavperianReportFound(context: any, event: any) {
+  // TODO: implement
+  return true;
+}
+
