3e67297657bc3cd120a149c936a83e86fdeaef82
Niraj <niraj38re@gmail.com>
2026-04-01 23:53:02 +0530

    feat: add assignCreditScoreError action to creditCheck state transitions

--- apps/fsm-core-example/fsm/creditCheck/v01/machine.ts
+++ apps/fsm-core-example/fsm/creditCheck/v01/machine.ts
@@ -1,40 +1,7 @@
-import { assign, createMachine, fromPromise, createActor, log, initialTransition } from 'xstate';
-// import { setup } from "xstate";
-
-// import {
-//   checkBureauService,
-//   checkReportsTable,
-//   determineMiddleScore,
-//   generateInterestRates,
-//   verifyCredentials,
-// } from "./actors/index";
-
-// import {
-//       assignSSN,
-//       assignFirstName,
-//       assignLastName,
-//       assignErrorMessage,
-//       assignEquiGavinScore,
-//       assignEquiGavinScoreFetch,
-//       assignGavUnionScore,
-//       assignGavUnionScoreFetch,
-//       assignGavperianScore,
-//       assignGavperianScoreFetch,
-//       assignMiddleScore,
-//       assignInterestRateOptions,
-//       assignFailedCreditScores,
-//       saveReportEquiGavin,
-//       saveReportGavUnion,
-//       saveReportGavperian,
-//       saveCreditProfile,
-//       emailUser,
-//       emailSalesTeam
-// } from './actions/index';
-
-// import  { allSucceeded, gavUnionReportFound, equiGavinReportFound, gavperianReportFound } from './guards/index';
+import { createMachine } from 'xstate';
 
 
-export const creditCheckMachine = createMachine({
+export const machine = createMachine({
   /** @xstate-layout N4IgpgJg5mDOIC5QFsCuAbALgSwA7rAGEAnSbTQgCzAGMBrAOhtInKtsYFEA7TMY7NygACAJLcAZgHtiyAIY4p3AMQBlVACNk5ANoAGALqJQuKbHLYlxkAA9EAZgAcAVgYB2ACwAmAIyO3zgA0IACeiD5ueu4AbM7RXs4AvonBaFh4BCRkFNT0TCxsuYwAavzYEiGCIlkQYLzYcuiwyhBKYAyCAG5SdO1pOPhEBTkc+dnseaUCFVXCNXU4jbAIXVI0Cpbc+gbb1qbmitzWdggAnB6u0Y72zqf2AcFhCAC0jgz20W5+AcmpGAOZYYTRjMcZFBhTcqVIRzFgLBpNZT8YgyBj4BTSWQMfoZIZg0ag1gjSZlGYw+b1JYrbjddaHba7JAgfYWKxMk56R6IPS-EA4wY1QoEoHg4FVQWYVQ0GRwFptBiwTAKPr-XES4FjIkasVCCVSmWwRkmMyso7sxBeDz+BgeU7OG56DwePTOZwuLkIa7RBiOFynPT2QNuaL2U6OXn8wH4vKEoV5HVQPXS0jNVrcdqK5XY1UCkXC6OMBNJg06HxGJksw7HRDOHyuV1uG5W+zeU5XD3PW7vRweOsfU5tl1eU4RnNRrXg2PEwtFcXDfUphgJzgAR1Q2AA4nJOoIl7OhAAxGScGzYRVVABKYFMxEwcvTHRpPXaYDXm+3ggAIgAhACCNEwGQjWZE0q3NBAAlOd5-ViV1HS8LxfQ9HxELeFDLRuAdh1ONxR3SXMC01OMZw4OdsgXOA91IoRV3XLcd24Kj6CqI9iBPM8cCEK8bzvNN2lWXoGFfOiP24H9-0A4hS3LY0Dk2asIK7UM9Fg5x4NQ5CnSieIMNuU5sNwlI+THPEJ3zMz433RN52TSjlzfejdwTVj2PPLjrxkO9kVRdFMExZAhIc0TxIAoDDD2UD5PAyDoJU101O8DTQnCN17HeNxdKwgdDL+fDx2IojpyYugyKJCjYGKqpaPfBiGAPMBMBoShLw829734p9BOEmrBHqxrKAksKZJAuS2VAE4fCtKDPjdHwUMiBJJs0vQvAYe1nC8Nw3EcaIZuiDw8IBUyCqnbUrOLRd7JE2q+qalqeKRYgUWINF0AxGQAu6xzuFugbQqk8KK0isbbHCW0ohbewfCh2s5r0O1NNdNaPltHtHEtbxDrVPMYxxkjmN1GyDUqoR6IAVW4TYSagFzTzcqBuM89rH26QSoG3CnNhCyTgMrKLxsQAJvUiOJfT0Rw9BW3bkO2nx3hbW5+y8PRIhy4y8uOorTtFc6icuqzycppRqdpjj7qZviWefBh2c6TmlG5oCywi0azQFxThZdK4JYlqXomQu40uuC47mifSVZVrGCIskE8epi67INjmjcY5zjzpziGdarynp8t6-I+m3k65v9-t54G3dBj33C9sXfa8aXkoQZXgzWntFbDluXSj-Ktbjos9cT6ioENqnfvNtrLYE9pbftn6GqawaAeGvmQYmjxYgYBJlLiHxznOf2m78AcGD37bPDbXs5pHIzI01jVtdGAfyNsiqE1H43x-ch7vJe3z-KLnbFOv0l7l1dgpSam99LoTdNEAMG03AB1tO4VWalHCnD8PYS0Pd76Tn7rrF+xN37blwGUOQqcrKm3pozSe8pp6ANIQIchwJQGA1kqaBS20oiug3v4eG4tQwem2q4S0-hMG+nQc4NWd91R4MIs-Mqr9qb0UYQ0Chw8qGZxobxOhnUZ4kLIdwFhZdnZA3AdFCWa1nC8MiP6a4pwPR3DcO4K0XxrgSLtNIkysjzIFQUeQcqyiDFMPUQTGm6czbfyZr-V670sS21UcwoorCV4VwgXFNaXgnBOhmr2QMyE6zOPiNECI-oAjeGhjgnxuN5EEMUUQpOnREmMS-lnB6U89FMCKPRAACoYlJLsOHgTrI6U+bg2x8LsYIpu-oPA2jDG4pwvpPFVLjo-Syw8E5v0ac0uqC9mpRLajE-+hcmocF6f0subCRpDPdiMuZpSri2IEQ4mZtYGADjEe45ZUjVmEXWYwT8DV+DaEpkIcQfAUyYAvMqAA8rgQ4FUgWQtBVUAAstgCAEACAUWZvQ6ezwNAQAAFboGQGA25VcsGfAYGHXaiE9BzSuIfJ4O1XCSycbA-wO1oh-JjoVDUyKQWCCqBC-gcBoVwoRZsCqrSYV8FTLo1mKoNbVNjoRIVsgRXgt4OKxU8qwDwsRXs-ql5lTLFWHSTYDJrmr0rice4e9aURBcC2HCGMPS+mcY2DwgYslSIQjcZIRluBSFqPAJkMi8aDLAu7Z4bYOxZLmdkvsYdBz2j5SdaNZjKUnGeFaDskCfQ9lTQOOBGbb7eLWXHHgkLZjiH8hsEGdrOFpRDIhFCQQj5dniEkStqrq2EUhGSaocJKRNBjfzKuJTvX2hgpNHw8CPAehuM450torQR0DLaTNfdambMHhG9hsaq4dvlk4JNYdbj+o9NY70ojFkeN+f2o6aqBU6wPYQ-Ww9qrfUnWvBww5z3oxbFe-SNxkLK20ghX1ekDK7ofvgz99Tv1hN-aJE2ETqHZ3-fahwW1gOXrtOBrtTw+zOPQRlOsVoULroQ3I-l-jJRKKuj1Fp+zxRSGQIMPguGIEZTeIy-1UyVI+GQjcdlK04hQwQjtH4L7sb-KQ2ErZ1N0M3Q44czAfHhnQ2mh8Tw-hrEZQRkfXsrh7gJUwQkBu9HfFFSY4E1j30TV3UPHIbABAIA6buV8Vays-C0Y7vYZC1i0pzRcMODKXx9p9tyq+wdjG6kBJY40uePmqX2CiMGRwfgfaSwbnEZCYcg4K1DuHVWdmalJeQylhpw8P6hJKoeLDWicM5pPQ67aMRct1wK7EFlFoAyrRUl8RsKs4ghiq+qmrKnD1BKAWPTTiYuM8bABliaXwoh3AbH58Zm1kLXDSkOFCLYVJhydNN99T9kvMfq2ExrrmDltM8ht8I18bROAypLe4+0LgB3Fm3L4A57R1hB1dgF8d5vEMW5-ZbB4PNebewgCIKEt6Mty5aDuiEA4EZuC2H1wYMEoQh8p5r1kv1Dwe8EtRyPAxpUtDBiWWV9ohabmHb1rjxE-K8QOpT+65uU+2Q1mn5DMNsQzhPbTHWp0Oty1vRKm6We+uQo6KIto71OiwVDSpCno5ZoF+T1TMPdmtMIKtggvGZcAZR54IOysZP8PsZ6kMCuvlLMkbzhL-PZtG+hzswxT2pfI7msgip-gQ4vMcdDN3j6eek8N6VOrqHycqMD60hHnnIAh6vafBIAYXTqSQkfAMUEH3c8kQdPXvdEMauBVqsFUAxVQoNUamVyPEJxFpXEDBekMpOg9Bdj5PY-WrsDXF9W3v+WQ81ainVkKJWt+lUoJF9e59QAxVinFtkO+zO72HMH7qB9NxcFEJNo+A3+oT-y2f2qm+6pb1K41crzV04uOF3sw54ZH+XSf135-tdL8g1q9cF7NBU1879m9F8n8ZUGADVYAekUQdxahvNrc8MEBQxXdO1do+8PUT8rRh9fVACMor9g0gA */
   context: {
     SSN: "",
@@ -269,10 +236,7 @@ export const creditCheckMachine = createMachine({
             },
             {
               target: "Entering Information",
-              actions: assign({
-                ErrorMessage: ({ context }) =>
-                  "Failed to retrieve credit scores.",
-              }),
+              actions: [{ type: "assignCreditScoreError" }],
             },
           ],
         },
@@ -320,51 +284,5 @@ export const creditCheckMachine = createMachine({
   },
 });
 
-export default creditCheckMachine;
+export default machine;
 
-
-// export const creditCheckMachineActor = setup({
-//   types: {
-//     events: {} as {
-//       type: "Submit";
-//       SSN: string;
-//       lastName: string;
-//       firstName: string;
-//     },
-//     context: {} as CreditProfile,
-//   },
-
-//   actors: {
-//     checkBureauService: fromPromise(
-//       async ({ input }: { input: { ssn: string; bureauName: string } }) =>
-//         await checkBureauService(input),
-//     ),
-//     checkReportsTable: fromPromise(
-//       async ({ input }: { input: { ssn: string; bureauName: string } }) =>
-//         await checkReportsTable(input),
-//     ),
-//     verifyCredentials: fromPromise(
-//       async ({ input }: { input: userCredential }) =>
-//         await verifyCredentials(input),
-//     ),
-//     determineMiddleScore: fromPromise(
-//       async ({ input }: { input: number[] }) =>
-//         await determineMiddleScore(input),
-//     ),
-//     generateInterestRates: fromPromise(
-//       async ({ input }: { input: number }) => await generateInterestRates(input),
-//     ),
-//   },
-//   actions: {
-//     saveReport: saveReportEquiGavin,
-//     emailUser: emailUser,
-//     saveCreditProfile: saveCreditProfile,
-//     emailSalesTeam: emailSalesTeam
-//   },
-//   guards: {
-//     allSucceeded: allSucceeded,
-//     gavUnionReportFound: gavUnionReportFound,
-//     equiGavinReportFound: equiGavinReportFound,
-//     gavperianReportFound: gavperianReportFound,
-//   },
-// }).createMachine(creditCheckMachine);
