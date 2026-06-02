9e2911ee3e0441b44d83907d19f8688a79553811
Niraj <niraj38re@gmail.com>
2026-06-02 15:18:33 +0530

    refactor: remove unnecessary schema creation and extension checks in fsm_core SQL

--- packages/database-src-extension/fsm_core/sql/fsm_core--1.0.sql
+++ packages/database-src-extension/fsm_core/sql/fsm_core--1.0.sql
@@ -1,22 +1,11 @@
-CREATE EXTENSION IF NOT EXISTS ltree;
-CREATE EXTENSION IF NOT EXISTS pgmq;
-
 ------------------------------------------------------------
 -- Schema, tables, records, privileges, indexes, etc
 ------------------------------------------------------------
--- When installed as an extension, we don't need to create the `fsm_core` schema
--- because it is automatically created by postgres due to being declared in
--- the extension control file
-DO
-$$
-BEGIN
-    IF (SELECT NOT EXISTS( SELECT 1 FROM pg_extension WHERE extname = 'fsm_core')) THEN
-      CREATE SCHEMA IF NOT EXISTS fsm_core;
-    END IF;
-END
-$$;
+-- fsm_core schema is automatically created by postgres because `schema = 'fsm_core'`
+-- is declared in the .control file. ltree and pgmq are loaded automatically via
+-- `requires = 'ltree, pgmq'` in the .control file before this SQL runs.
 
-CREATE FUNCTION fsm_core.hello(input_text TEXT)
+CREATE FUNCTION fsm_core.hello_world(input_text TEXT)
 RETURNS void AS $$
 BEGIN
     RAISE NOTICE 'Hello, %!', input_text;
