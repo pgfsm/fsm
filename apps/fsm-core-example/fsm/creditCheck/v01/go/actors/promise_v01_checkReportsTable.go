6eb19a70340a8f38d52d326dca055052756ab840
Niraj <niraj38re@gmail.com>
2026-07-06 22:37:21 +0530

    refactor(fsm-core-example): remove unused checkReportsTable and checkBureau actor stubs

--- apps/fsm-core-example/fsm/creditCheck/v01/go/actors/promise_v01_checkReportsTable.go
+++
@@ -1,7 +0,0 @@
-package actors
-
-// Actor: checkReportsTable
-func checkReportsTable(context map[string]any, event map[string]any) {
-	// TODO: implement actor logic
-}
-
--- apps/fsm-core-example/fsm/creditCheck/v01/rust/actors/promise_v01_checkBureau.rs
+++
@@ -1,5 +0,0 @@
-// Actor: checkBureau
-pub fn checkBureau(context: &serde_json::Value, event: &serde_json::Value) {
-    // TODO: implement actor logic
-}
-
